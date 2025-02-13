2338939b84be4057e0773521f4c649ad60bf3016
ryuji koyama <ryujikoyama95@gmail.com>
2025-02-13 22:04:12 +0900

    Add agora config

---
+++ app/src/main/java/com/eotw95/agorademo/AgoraConfig.kt
@@ -0,0 +1,6 @@
+package com.eotw95.agorademo
+
+const val APP_ID = "5e35bd78186f4dd39dadb03fb462949c"
+const val CERTIFICATE = "c26dc721083b42c4b010c3aa0d213f41"
+const val TOKEN = "007eJxTYPDiOr9viZfTodD5bZ1pTG+Ofds4SU7n/PnDsTPEDtbN3SmpwGCaamyalGJuYWhhlmaSkmJsmZKYkmRgnJZkYmZkaWKZvPrp2vSGQEaGJhkdRkYGCATxuRlcUnPznTMS8/JScxgYAJjvIy0="
+const val CHANNEL_NAME = "DemoChannel"
